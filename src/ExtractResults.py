3d939a97e2a6fcee282b3b39666df51ed9bc06f0
mel1ssa <melissa.mekaoui@gmail.com>
2017-05-05 19:46:51 +0200

    97 %!!!

--- src/ExtractResults.py
+++ src/ExtractResults.py
@@ -1,6 +1,21 @@
 import os.path
 import sys
+from operator import truediv
 
+def verif_result(input):
+    input_file = open(input, 'r')
+    justepos=0
+    i=0
+    for line in input_file:
+        i+=1
+        if i<=2000:
+            if  int(line)==-1:
+                justepos+=1
+        else:
+            if  int(line)==1:
+                justepos+=1
+
+    print("Il y a "+str( truediv(justepos,4000))+" commen correctes")
 
 def extract(input_path, output_path):
     input_file = open(input_path, 'r')
@@ -33,4 +48,4 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+   verif_result(sys.argv[2])
